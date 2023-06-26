5758149fb18a95e1a9e7fb44f8b18206321992ff
Roman <kurpelroma@gmail.com>
2023-06-26 13:19:06 +0300

    add formik in forms in basket

--- src/pages/Basket/Basket.tsx
+++ src/pages/Basket/Basket.tsx
@@ -1,42 +1,88 @@
 import React from 'react';
-
-import BasketData from './sections/BasketData'
-import Shipping from './sections/Shipping';
-import BasketSideBar from './sections/BasketSideBar';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../redux/store';
-import BasketProductCard from './sections/BasketProductCard';
 import { useTranslation } from 'react-i18next';
-const Basket: React.FC = () => {
-    const {t} = useTranslation()
-    const {products,totalCount} = useSelector((state:RootState)=>state.basket)
-    return (
-        <div className='basket__wrapper'>
-            <div className="container basket__container">
-                <section className="basket">
-                    <div className="basket__login">
-                        <div className="basket__login-content">
-                            <h5 className="basket__login-title">{t('basket__login-title')}</h5>
-                            <span className="basket__login-text">{t('basket__login-text')}</span>
-                        </div>
-                        <button className="button basket__login-btn">{t('basket__login-btn')}</button>
-                    </div>
-                    <div className="basket__order">
-                        <div className="basket__order-top">
-                            <h4 className="basket__order-top-title">{t('basket__order-top-title')}</h4>
-                            <span className="basket__order-top-count">{t('basket__order-top-count1')} {totalCount} {t('basket__order-top-count2')}</span>
-                        </div>
-                        <ul className="basket__order-list">
-                            {products.map((item, index) => <BasketProductCard key={index} item={{...item}}/>)}
-                        </ul>
-                    </div>
+import { valuesDataForm } from '../../types/types';
+import { Formik } from 'formik';
 
-                </section>
-                <BasketData />
-                <Shipping />
-            </div>
-            <BasketSideBar />
-        </div>
-    )
+import BasketData from './sections/BasketData';
+import BasketSideBar from './sections/BasketSideBar';
+import BasketProductCard from './sections/BasketProductCard';
+import Shipping from './sections/Shipping';
+
+const Basket: React.FC = () => {
+    const { t } = useTranslation();
+    const { products, totalCount } = useSelector((state: RootState) => state.basket);
+
+    const initialValuesData: valuesDataForm = {
+        name: "",
+        number: "",
+        coment: "",
+        address: "",
+        entrance: "",
+        apartment: "",
+        floor: ""
+    };
+    const handleSubmit = (values: valuesDataForm) => {
+        let isValid = true;
+        if (products.length < 1) {
+            alert(t("noProductSelected"))
+            return
+        }
+        for (const key in values) {
+            const value: string = values[key as keyof typeof values] as string;
+            if (!value) {
+                isValid = false;
+                alert(t("fillAllFields"));
+                break;
+            }
+        }
+
+        if (isValid) {
+            alert(t("orderSubmitted"));
+        }
+    };
+
+
+    return (
+        <Formik
+            initialValues={initialValuesData}
+            onSubmit={handleSubmit}
+        >
+            {({ values, handleChange, handleSubmit }) => (
+                <form onSubmit={handleSubmit}>
+                    <div className='basket__wrapper'>
+                        <div className="container basket__container">
+                            <section className="basket">
+                                <div className="basket__login">
+                                    <div className="basket__login-content">
+                                        <h5 className="basket__login-title">{t('basket__login-title')}</h5>
+                                        <span className="basket__login-text">{t('basket__login-text')}</span>
+                                    </div>
+                                    <button className="button basket__login-btn">{t('basket__login-btn')}</button>
+                                </div>
+                                <div className="basket__order">
+                                    <div className="basket__order-top">
+                                        <h4 className="basket__order-top-title">{t('basket__order-top-title')}</h4>
+                                        <span className="basket__order-top-count">{t('basket__order-top-count1')} {totalCount} {t('basket__order-top-count2')}</span>
+                                    </div>
+                                    <ul className="basket__order-list">
+                                        {products.map((item, index) => <BasketProductCard key={index} item={{ ...item }} />)}
+                                    </ul>
+                                </div>
+                            </section>
+                            <BasketData values={values} handleChange={handleChange} />
+                            <Shipping values={values} handleChange={handleChange} />
+                        </div>
+                        <BasketSideBar />
+                    </div>
+                </form>
+
+            )}
+        </Formik>
+
+
+    );
 }
 export default Basket
+
--- src/pages/Basket/sections/Shipping.tsx
+++ src/pages/Basket/sections/Shipping.tsx
@@ -1,32 +1,62 @@
 import { useTranslation } from "react-i18next"
-
-const Shipping = () => {
-    const {t} = useTranslation()
+import { valuesDataForm } from "../../../types/types";
+interface BasketDataProps {
+    values: valuesDataForm;
+    handleChange: (e: React.ChangeEvent<any>) => void;
+}
+const Shipping: React.FC<BasketDataProps> = ({ values, handleChange }) => {
+    const { t } = useTranslation()
     return (
         <section className="shipping">
             <div className="shipping__types">
                 <span className="shipping__type shipping__type-left shipping__type-active--type">{t('shipping__type-left')}</span>
                 <span className="shipping__type shipping__type-right">{t('shipping__type-right')}</span>
             </div>
-            <form className="data--form">
+            <div className="data--form">
                 <label className="data--label">
-                    <input className="data--input shipping__input" type="text" placeholder={t("shipping__input1")as string} />
+                    <input
+                        className="data--input shipping__input"
+                        value={values.address}
+                        onChange={handleChange}
+                        name="address"
+                        type="text"
+                        placeholder={t("shipping__input1") as string}
+                    />
                     {t('data--label4')}
                 </label>
                 <label className="data--label">
-                    <input className="data--input shipping__input" type="text" placeholder={t("shipping__input2")as string} />
+                    <input
+                        className="data--input shipping__input"
+                        type="text"
+                        placeholder={t("shipping__input2") as string}
+                        value={values.entrance}
+                        name="entrance"
+                        onChange={handleChange}
+                    />
                     {t('data--label5')}
                 </label>
                 <label className="data--label">
-                    <input className="data--input shipping__input" type="text" placeholder={t("shipping__input3")as string} />
+                    <input className="data--input shipping__input"
+                        type="text"
+                        name="apartment"
+                        placeholder={t("shipping__input3") as string}
+                        value={values.apartment}
+                        onChange={handleChange}
+                    />
                     {t('data--label6')}
                 </label>
                 <label className="data--label">
-                    <input className="data--input shipping__input" type="text" placeholder={t("shipping__input4")as string} />
+                    <input
+                        className="data--input shipping__input"
+                        type="text"
+                        placeholder={t("shipping__input4") as string}
+                        name="floor"
+                        value={values.floor}
+                        onChange={handleChange}
+                    />
                     {t('data--label7')}
-
                 </label>
-            </form>
+            </div>
         </section>
     )
 }
